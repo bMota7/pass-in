1fcf37a6b68d15d7167b9c9df30b380817f1ea03
Bruno Mota <bruno7motadev@gmail.com>
2024-04-02 17:03:29 -0300

    feat: created metodo getEventDetail

--- src/main/java/bruno/com/passin/services/EventService.java
+++ src/main/java/bruno/com/passin/services/EventService.java
@@ -1,17 +1,24 @@
 package bruno.com.passin.services;
 
+import bruno.com.passin.domain.attendee.Attendee;
 import bruno.com.passin.domain.event.Event;
+import bruno.com.passin.dto.event.EventResponseDTO;
+import bruno.com.passin.repositories.AttendeeRepository;
 import bruno.com.passin.repositories.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EventService {
     private final EventRepository eventRepository;
+    private final AttendeeRepository attendeeRepository;
 
-    public void getEventDetail(String eventId) {
+    public EventResponseDTO getEventDetail(String eventId) {
         Event event = this.eventRepository.findById(eventId).orElseThrow(() -> new RuntimeException("Event not found with ID:" + eventId));
-        return;
+        List<Attendee> attendeeList = this.attendeeRepository.findByEventId(eventId);
+        return new EventResponseDTO(event, attendeeList.size());
     }
 }
